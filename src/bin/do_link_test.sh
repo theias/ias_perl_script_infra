d02b73007edc7d9dc6765d79e81e9357d088aec5
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-11-27 19:54:15 -0500

    shellcheck.  unsure if still needed.

--- src/bin/do_link_test.sh
+++ src/bin/do_link_test.sh
@@ -7,15 +7,15 @@ cp config_app_test.pl root_config_app_test.pl
 
 mkdir link_test
 
-cd link_test
+cd link_test || exit 1
 
 ln -s ../root_config_app_test.pl ./root_config_app_test.pl
 
-chown $1 ./root_config_app_test.pl
+chown "$1" ./root_config_app_test.pl
 
-su $1 -c './root_config_app_test.pl --log-devel'
+su "$1" -c './root_config_app_test.pl --log-devel'
 
-cd ../
+cd ../ || exit 1
 
 rm -rf link_test
 rm -f root_config_app_test.pl
